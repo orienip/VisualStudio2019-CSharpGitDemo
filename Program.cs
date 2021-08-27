d7482003079ab8bfd23789dad629f5ee7cd53dec
orien <orien@DESKTOP-IFT3A17>
2021-08-27 18:18:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VisualStudio2019_CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
